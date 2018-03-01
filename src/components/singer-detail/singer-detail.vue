a50ff5e4fa03975533935dee1dbb09452353256d
shandl <shandongliangwy@163.com>
2018-03-01 17:21:20 +0800

    'music-list开发'

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,10 +1,10 @@
 <template>
 <transition name="slide">
-  <div class="singer-detail"></div>
+  <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
 </transition>
 </template>
 <script type="text/ecmascript-6">
-
+import MusicList from 'components/music-list/music-list'
 import {mapGetters} from 'vuex'
 import {getSingerDetail} from 'api/singer'
 import {ERR_OK} from 'api/config'
@@ -13,10 +13,16 @@ import {createSong} from 'common/js/song'
 export default{
   data() {
     return {
-      song: []
+      songs: []
     }
   },
   computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
@@ -48,6 +54,9 @@ export default{
       })
       return ret
     }
+  },
+  components: {
+    MusicList
   }
 }
 
@@ -55,15 +64,6 @@ export default{
 
 <style lang="stylus" scoped rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position : fixed
-    z-index : 100
-    top : 0
-    left : 0
-    right : 0
-    bottom : 0
-    background:$color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
