6a85de371a67f2fb7a5ee953bcdc431d871782ed
shandongliang <shandongliangwy@163.com>
2018-06-27 11:06:45 +0800

    shan

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="song-list">
     <ul>
-      <li class="item" v-for="song in songs">
+      <li class="item" v-for="song in songs" :key="song">
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
